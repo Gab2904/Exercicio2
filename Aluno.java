25855a5786a1a95cea294e446464bc72077165ff
Gab2904 <gabriel.r.martins@outlook.com>
2020-03-10 01:53:24 -0300

    Versao 2

--- Aluno.java
+++ Aluno.java
@@ -3,4 +3,14 @@ public class Aluno{
    private String nome;
    private int id;
    
+   
+   
+public String getNome() {
+	return nome;
+}
+public void setNome(String nome) {
+	this.nome = nome;
+}
+
+   
 }
